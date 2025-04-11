5beb4ed4498bf72a6588e2d37cd8fb12d8d7c5c8
AceByte <brambramdk@gmail.com>
2025-04-11 13:17:55 +0200

    Remove unused quest display logic and clean up OnBoat and Lilbuddyman scripts

--- Scripts/Onboat.gd
+++ Scripts/Onboat.gd
@@ -8,12 +8,6 @@ func _ready():
 		printerr("Error: 'Lilbuddyman' node not found!")
 	else:
 		print("onboat: Lilbuddyman found.")
-		#original_position = boat_character.position
-		#print("Original position of lilbuddyman:", original_position)
-
-	#var quest = preload("res://Quest System/20_fish.tres")
-	#$UI/QuestDisplay.set_quest(quest)
-	#$UI/QuestDisplay.update_from_db(quest.fish_column)
 
 func _process(_delta):
 	pass
